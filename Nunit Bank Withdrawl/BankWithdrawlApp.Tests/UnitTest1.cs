51c5ad70603bf675be1f875dbeb04fd0b26525bc
mani-shashi <shashi2043@gmail.com>
2026-02-03 15:57:23 +0530

    updated Project: Nunit Test Cases - Bank Withdrawl

---
+++ Nunit Bank Withdrawl/BankWithdrawlApp.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace BankWithdrawlApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test_Deposit_NegativeAmount()
+    {
+        Program account = new Program(1000m);
+
+        Assert.AreEqual(
+                "Deposit amount cannot be negative",
+                Assert.Throws<Exception>(() => account.Deposit(-100m)).Message
+        );
+    }
+}
---
+++ Nunit Bank Withdrawl/BankWithdrawlApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Program
+{
+    public decimal Balance { get; set; }
+    public Program(decimal balance)
+    {
+        this.Balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new Exception("Balance can't be zero or negative");
+        }
+        Balance+=amount;
+            
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            throw new Exception("Amount is greater than Balance");
+                
+        }
+        Balance -= amount;
+    }
+}
