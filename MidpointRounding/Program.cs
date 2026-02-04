d949de42b7d533915a9b0fa83a4cc75362f5fc17
mani-shashi <shashi2043@gmail.com>
2026-02-04 15:40:00 +0530

    updated

---
+++ MidpointRounding/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Enter radius: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        double area = CalculateArea(radius);
+        Console.Write($"Area : {area}");
+    }
+
+    public static double CalculateArea(double radius)
+    {
+        double area = Math.PI * radius * radius;
+        return Math.Round(area,2);
+    }
+}
